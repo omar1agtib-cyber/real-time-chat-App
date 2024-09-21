427d91181ca2f4303b5213d2e2085b6aa2e11a55
agtib omar <oagtib@gmail.com>
2024-09-21 17:18:27 +0100

    implement login logique

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
@@ -9,7 +20,7 @@ const Login = () => {
           Login <span className="text-blue-500">ChatVibe</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base text-gray-100">Username</span>
@@ -18,6 +29,8 @@ const Login = () => {
               type="text"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10"
+              value={username}
+							onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -29,6 +42,8 @@ const Login = () => {
               type="text"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -40,8 +55,15 @@ const Login = () => {
           </Link>
 
           <div>
-            <button className="btn btn-sm mt-2 border-slate-700 bg-blue-500">
-              Login
+            <button
+              className="btn btn-sm mt-2 border-slate-700 bg-blue-500"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="text-white loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
         </form>
